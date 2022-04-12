467c7c6427a6c0baedcf0360ae3ad7a489f0ead7
Dghost971 <76606507+Dghost971@users.noreply.github.com>
2022-04-12 04:55:39 +0200

    gene

--- passw_gen.py
+++ passw_gen.py
@@ -7,13 +7,13 @@ special_char=list("!@#$%&()£")
 characters = list(string.ascii_letters+string.digits+"!@#$%&()£")
 
 def generate_random_passwd():
-    length=int(input("Entrer la taille du password: "))
+    length=int(input("Entrer la taille du mot de passe: "))
     alphabets_count=int(input("Entrer le nombre de lettres que vous voulez: "))
     digits_count=int(input("Entrer le nombre de chiffres: "))
     special_char_count=int(input("Entrer le nombre de character spéciaux: "))
     characters_count=alphabets_count+digits_count+special_char_count
     if characters_count>length:
-        print("Le nombre total de caractères est supérieur à la longueur du password.")
+        print("Le nombre total de caractères est supérieur à la longueur du mot de passe.")
         return 
     passwd=[]
     for i in range(alphabets_count):
@@ -27,7 +27,7 @@ def generate_random_passwd():
         for i in range(length-characters_count):
             passwd.append(random.choice(characters))
     random.shuffle(passwd)
-    print("Voici votre password: ") 
+    print("Voici votre mot de passe: ") 
     print("".join(passwd))
     
 generate_random_passwd()
